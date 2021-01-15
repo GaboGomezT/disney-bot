8046eaf3f05b4b2d93474baf86090938c9ab9426
Gabriel <gomezgabriel09@gmail.com>
2021-01-14 19:34:18 -0600

    removed creds

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 import csv
 from datetime import date
 
-account_sid = "AC972791418f909d0dfb8e6d3939fb83d0"
-auth_token = "833b7bdc3e4e5fb707b183033a04deef"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 STARTING_DATE = date(2020, 5, 11)
@@ -23,10 +23,10 @@ def send_love(event, context):
         if phrases_left <= 5:
             alert_message = f"Faltan {phrases_left} día(s) para renovar disney-bot de Viri."
             client.messages.create(
-                to="+522491637747", from_="+13237161581", body=alert_message
+                to="+", from_="+", body=alert_message
             )
         formatted_phrase = f"Con amor, de Gabo\n\n{chosen_phrase[0]} - {chosen_phrase[1]}"
         client.messages.create(
-            to="+525514990083", from_="+13237161581", body=formatted_phrase
+            to="+", from_="+", body=formatted_phrase
         )
     print("Love sent!")
